a9edb8589b1ed97d0ec681294fd1db7b89671a09
eduardo <eduardomachadorezende0@gmail.com>
2021-03-05 21:52:31 -0300

    Delete

--- src/main/java/app/car/cap01/domain/TravelRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package app.car.cap01.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TravelRequestRepository extends JpaRepository<TravelRequest, Long> {
-
-}
